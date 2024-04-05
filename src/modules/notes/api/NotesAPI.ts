d291a0f344ec9fb34a7c3f0cbc8ca882b5dcc221
bohdanda <reborn.muffin@gmail.com>
2024-04-05 22:43:40 +0300

    fix of typo

--- src/modules/notes/api/NotesAPI.ts
+++ src/modules/notes/api/NotesAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {INote} from '../types/notes';
 
-const BASE_URL = 'http://loc2alhost:3001';
+const BASE_URL = 'http://localhost:3001';
 
 class NotesAPI {
   static async fetchAll() {
